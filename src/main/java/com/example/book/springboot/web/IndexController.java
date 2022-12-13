e5d04b0389413e6b14bd7fa9bd1a8e1ac5e54283
Jeeho Kim <rlawlgh3245@gmail.com>
2022-12-13 19:45:30 +0900

    12/13

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.book.springboot.web;
 
+import com.example.book.springboot.config.auth.LoginUser;
+import com.example.book.springboot.config.auth.dto.SessionUser;
 import com.example.book.springboot.service.posts.PostsService;
 import com.example.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,22 +10,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // 전체 조회 이동
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
+
     // 등록 페이지 이동
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
+
     //수정 상세 페이지 이동
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
@@ -32,3 +42,5 @@ public class IndexController {
         return "posts-update";
     }
 }
+
+
