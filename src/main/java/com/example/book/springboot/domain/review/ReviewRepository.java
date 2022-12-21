976c20bd05be6e55e38689751cb7be2aa6d60959
Jeeho Kim <rlawlgh3245@gmail.com>
2022-12-21 15:49:15 +0900

    12/20

--- src/main/java/com/example/book/springboot/domain/review/ReviewRepository.java
+++ src/main/java/com/example/book/springboot/domain/review/ReviewRepository.java
@@ -1,4 +1,6 @@
 package com.example.book.springboot.domain.review;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
--- src/main/java/com/example/book/springboot/domain/wishlist/WishlistRepository.java
+++ src/main/java/com/example/book/springboot/domain/wishlist/WishlistRepository.java
@@ -1,4 +1,6 @@
 package com.example.book.springboot.domain.wishlist;
 
-public interface WishlistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
 }
