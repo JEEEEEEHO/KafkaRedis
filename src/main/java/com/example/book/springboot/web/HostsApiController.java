cd2ea61ac3fbb3e6b8555009829f14f87ca15ad5
Jeeho Kim <rlawlgh3245@gmail.com>
2022-12-28 15:48:38 +0900

    12/28

---
+++ src/main/java/com/example/book/springboot/web/HostsApiController.java
@@ -0,0 +1,9 @@
+package com.example.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HostsApiController {
+}
